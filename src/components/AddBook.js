9f2821cfe63be48bab83764fe820739fb0b8df4e
yahya <crazy20046@gmail.com>
2021-11-03 04:37:52 +0200

    Config store

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -3,6 +3,7 @@ import React from 'react';
 const AddBook = () => (
   <div>
     <input type="text" placeholder="new book" />
+    <input type="text" placeholder="Author" />
     <select id="cars" name="cars">
       <option>Action</option>
       <option>Comedy</option>
